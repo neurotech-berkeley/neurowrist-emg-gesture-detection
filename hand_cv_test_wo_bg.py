ea2951d2e5d09ea65eb7b95961ade4a92dd11d09
reubenkthomas@gmail.com <74014913+ReubenkThomas@users.noreply.github.com>
2024-06-11 14:54:01 -0700

    work on recording landmarks into a data store format with time stamps

--- hand_cv_test_wo_bg.py
+++ hand_cv_test_wo_bg.py
@@ -1,6 +1,8 @@
 import cv2
 import mediapipe as mp
 import time
+import numpy as np
+
 
 frameWidth = 1280
 frameHeight = 720
@@ -14,12 +16,18 @@ mpDraw = mp.solutions.drawing_utils
 pTime = 0
 cTime = 0
 
+connection_drawing_spec = mpDraw.DrawingSpec(color=(0, 255, 0), thickness=2, circle_radius=2)
+
 while True:
     success, img = cap.read()
-    # img= cv2.flip(img,1)
+    img= cv2.flip(img,1)
+    img_w = np.empty(img.shape)
+    img_w.fill(255)
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
     #print(results.multi_hand_landmarks)
+
+
 
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks: 
@@ -32,15 +40,21 @@ while True:
                 cx, cy = int(lm.x * w), int(lm.y * h)
                 #print(id, cx, cy)
                 # if id == 4: #(To draw 4th point)
-                #cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
-            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS) #drawing points and lines(=handconections)
+                cv2.circle(img_w, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+            mpDraw.draw_landmarks(
+                img_w, 
+                handLms, 
+                mpHands.HAND_CONNECTIONS,
+                mpDraw.DrawingSpec(color=(255, 0, 255), thickness=2, circle_radius=2),
+                connection_drawing_spec
+            ) #drawing points and lines(=handconections)
 
     #Write frame rate
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
-    cv2.putText(img, "FPS= " + str(int(fps)), (10, 20), cv2.FONT_HERSHEY_PLAIN, 1,(0, 0, 0), 1)
+    cv2.putText(img_w, "FPS= " + str(int(fps)), (10, 20), cv2.FONT_HERSHEY_PLAIN, 1,(0, 0, 0), 1)
     
-    cv2.imshow('image', img)
+    cv2.imshow('image', img_w)
     if cv2.waitKey(1)==27:
         break
