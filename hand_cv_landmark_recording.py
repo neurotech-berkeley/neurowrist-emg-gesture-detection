376b26da9a1ba34882be66e86ef23757b469846d
reubenkthomas@gmail.com <74014913+ReubenkThomas@users.noreply.github.com>
2024-06-11 16:57:44 -0700

    finished handlandmark coordinate saving

--- hand_cv_landmark_recording.py
+++ hand_cv_landmark_recording.py
@@ -1,8 +1,8 @@
-import numpy as np
 import cv2
 from time import time
 import mediapipe as mp
 import numpy as np
+import json
 
 cap = cv2.VideoCapture(0) # Capture video from camera
 
@@ -18,21 +18,28 @@ fourcc = cv2.VideoWriter_fourcc(*'mp4v') # Be sure to use the lower case
 out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (width, height))
 
 
-# 
+# initialize media pipe model to detect hand movement 
+mpHands=mp.solutions.hands
+hands=mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+connection_drawing_spec = mpDraw.DrawingSpec(color=(0, 255, 0), thickness=2, circle_radius=2)
 
-
-
-# initializtion for timesteps for the video
+# initializtion for timesteps list for the video
 first_time = None
 video_timestamps = []
+
+# initialization for landmark list. Non-standard height and width
+landmarks_v_time = [] 
+
+
 
 while(cap.isOpened()):
     ret, frame = cap.read()
     if ret == True:
-        frame = cv2.flip(frame,0)
+        frame = cv2.flip(frame,1)
 
-        # write the flipped frame
-        out.write(frame)
+        imgRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # process to RGB form
+        results = hands.process(imgRGB) # result of hand gesture decoding
 
         # record and save timestamps
         if not first_time:
@@ -40,6 +47,29 @@ while(cap.isOpened()):
             video_timestamps.append(0)
         else:
             video_timestamps.append(time() - first_time)
+
+        
+        # save the gesture points
+        if results.multi_hand_landmarks:
+            a = []
+            for handLms in results.multi_hand_landmarks: 
+                b = []
+                for id, lm in enumerate(handLms.landmark):
+                    h, w, c =frame.shape
+                    cx, cy = int(lm.x * w), int(lm.y * h)
+                    b.append([cx, cy])
+                    cv2.circle(frame, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+                    mpDraw.draw_landmarks(
+                        frame, 
+                        handLms, 
+                        mpHands.HAND_CONNECTIONS,
+                        mpDraw.DrawingSpec(color=(255, 0, 255), thickness=2, circle_radius=2),
+                        connection_drawing_spec
+                    )
+                a.append(b)
+            landmarks_v_time.append(a)
+
+        
 
         cv2.imshow('frame',frame)
         if (cv2.waitKey(1) & 0xFF) == ord('q'): # Hit `q` to exit
@@ -52,5 +82,11 @@ out.release()
 cap.release()
 cv2.destroyAllWindows()
 
-timestamps_array = np.array(video_timestamps)
-np.save('video_timestamps.npy', timestamps_array)
+
+landmarks_data = {
+    'timestamps': video_timestamps,
+    'landmarks': landmarks_v_time
+}
+
+with open('landmark_data.json', 'w') as json_file:
+    json.dump(landmarks_data, json_file)
